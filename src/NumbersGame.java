6c1c36135fb7626ef84852449d5a380699574b53
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-23 10:50:09 +0200

    poprawa zadania

--- src/NumbersGame.java
+++ src/NumbersGame.java
@@ -4,25 +4,22 @@ public class NumbersGame {
     private Scanner sc = new Scanner(System.in);
 
     public void doesItBelongToCompartment() {
-        while (true) {
-            int x = setX();
-            if ((x >= 100 && x <= 200) && (x % 3) == 0) {
-                System.out.println("Udalo Ci się trafić!");
-                break;
-            } else if (x < 100) {
-                System.out.print("Lipa... Podana liczba jest za mała. ");
-            } else if ((x >= 100 && x <= 200) && (x % 3) != 0) {
-                System.out.print("Lipa... Podana liczba jest w przedziale, ale nie jest podzielna przez 3. ");
-            } else {
-                System.out.println("Lipa... Podana liczba jest za duża. ");
+        int x = 0;
+        do {
+            x = getXFromUser();
+            if (x < 100) {
+                System.out.println("Podana liczba jest za mała... ");
+            } else if (x > 200) {
+                System.out.println("Podana liczba jest za duża... ");
+            } else if (x % 3 != 0) {
+                System.out.println("Przedział dobry, ale liczba jest niepodzielna przez trzy... ");
             }
-        }
+        } while (!((x >= 100 && x <= 200) && (x % 3) == 0));
+        System.out.println("Udało Ci się! Liczba mieści się w przedziałe i jest podzielna przez trzy! ");
     }
 
-    private int setX() {
+    private int getXFromUser() {
         System.out.print("Podaj liczbę: ");
         return sc.nextInt();
     }
-
-
 }
